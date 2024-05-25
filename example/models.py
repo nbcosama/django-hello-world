addf493bfa4c18d2f3f96eae9767c650b8bb6d73
DevelopAamir <mdamirten@gmail.com>
2024-05-26 01:29:49 +0545

    done

--- example/models.py
+++ example/models.py
@@ -5,6 +5,7 @@ class Supply(models.Model):
     item_name = models.CharField(max_length=100)
     quantity = models.PositiveIntegerField()
     supplier = models.CharField(max_length=100)
+    supplyer_name = models.CharField(max_length=100, default='N/A')
 
     def __str__(self):
         return self.item_name
